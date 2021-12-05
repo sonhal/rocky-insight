60d0b154cf8c8cefcf054fba8e54d1a0473d9b02
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2021-12-05 18:22:25 +0100

    Imp function for creating SSL consumer

--- src/rapids_and_rivers/kafka.rs
+++ src/rapids_and_rivers/kafka.rs
@@ -1,11 +1,5 @@
-use std::process::exit;
-use std::sync::Arc;
-use rdkafka::{ClientConfig, ClientContext, Message};
-use rdkafka::config::RDKafkaLogLevel;
-use rdkafka::consumer::{BaseConsumer, Consumer, ConsumerContext, DefaultConsumerContext, StreamConsumer};
-use rdkafka::error::{KafkaError, KafkaResult};
-use rdkafka::util::Timeout;
-use rdkafka::util::Timeout::After;
+use rdkafka::consumer::{Consumer, ConsumerContext, DefaultConsumerContext, StreamConsumer};
+use rdkafka::ClientConfig;
 
 pub(crate) type RapidConsumer = StreamConsumer<DefaultConsumerContext>;
 
@@ -22,7 +16,37 @@ pub fn create_consumer(bootstrap_servers: &str, topic: &str) -> RapidConsumer {
         .create()
         .expect("Consumer creation failed");
 
-    consumer.subscribe(&[topic]).expect("Error while subscribing");
+    consumer
+        .subscribe(&[topic])
+        .expect("Error while subscribing");
     return consumer;
 }
 
+pub fn create_ssl_consumer(
+    bootstrap_servers: &str,
+    topic: &str,
+    ca_location: &str,
+    cert_location: &str,
+    key_location: &str,
+    key_password: &str,
+) -> RapidConsumer {
+    let consumer: RapidConsumer = ClientConfig::new()
+        .set("group.id", GROUP_ID)
+        .set("bootstrap.servers", bootstrap_servers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "false")
+        .set("auto.offset.reset", "earliest")
+        .set("security.protocol", "SSL")
+        .set("ssl.ca.location", ca_location)
+        .set("ssl.certificate.location", cert_location)
+        .set("ssl.key.location", key_location)
+        .set("ssl.key.password", key_password)
+        .create()
+        .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&[topic])
+        .expect("Error while subscribing");
+    return consumer;
+}
